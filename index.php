338ee0c1f700c81eaff1dc3fc6815bbc7c9a723e
sammut91 <sammut91@gmail.com>
2017-09-20 17:05:16 +1000

    Fix typos

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 <body>
 <form  action="welcome.php" method="post">
 
-FisrtName: <input type="text" name="firstname" value="<?php echo $firstname;?>"><br/><br/>
-LastName:  <input type="text" name="lastname" value="<?php echo $lastname;?>"><br/><br/>
+First Name: <input type="text" name="firstname" value="<?php echo $firstname;?>"><br/><br/>
+Las tName:  <input type="text" name="lastname" value="<?php echo $lastname;?>"><br/><br/>
 Number of Tyres: <input type="number" name="tyres" value="<?php echo $tyres;?>"><br/><br/>
 <input type="submit" name="Calculate"><br/>
 
