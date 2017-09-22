d2b8e7dcd6b65338db28c8e400edf0f758036c02
sammut91 <sammut91@gmail.com>
2017-09-22 11:54:51 +1000

    update shit

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
-
-<html>
-<head>
-<title> Bob's Auto Parts</title>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
-</head>
-
-<body>
-<form  action="welcome.php" method="post">
-
-First Name: <input type="text" name="firstname" value="<?php echo $firstname;?>"><br/><br/>
-Last Name:  <input type="text" name="lastname" value="<?php echo $lastname;?>"><br/><br/>
-Number of Tyres: <input type="number" name="tyres" value="<?php echo $tyres;?>"><br/><br/>
-<input type="submit" name="Calculate"><br/>
-
-</form>
-</body>
-</html>
+ <html>
+ <head>
+ <Title>Registration Form</Title>
+ <style type="text/css">
+ 	body { background-color: #fff; border-top: solid 10px #000;
+ 	    color: #333; font-size: .85em; margin: 20; padding: 20;
+ 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+ 	}
+ 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+ 	h1 { font-size: 2em; }
+ 	h2 { font-size: 1.75em; }
+ 	h3 { font-size: 1.2em; }
+ 	table { margin-top: 0.75em; }
+ 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+ 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+ </style>
+ </head>
+ <body>
+ <h1>Register here!</h1>
+ <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+ <form  action="welcome.php" method="post">
+ 
+    First Name: <input type="text" name="firstname" value="<?php echo $firstname;?>"><br/><br/>
+    Last Name:  <input type="text" name="lastname" value="<?php echo $lastname;?>"><br/><br/>
+    Number of Tyres: <input type="number" name="tyres" value="<?php echo $tyres;?>"><br/><br/>
+    <input type="submit" name="Calculate"><br/>
+ 
+ </form>
+ </body>
+ </html>
