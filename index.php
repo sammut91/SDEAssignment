211bbbf0cc619ce0bb9ba185bbda1002d4c63205
Luke Mackie <9522654@student.swin.edu.au>
2017-09-26 16:50:54 +1000

    more more more.. more? css

--- index.php
+++ index.php
@@ -6,27 +6,26 @@
  	    color: #333; font-size: .85em; margin: 20; padding: 20;
  	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
  	}
-	h1 { color: #00688b; margin-bottom: 0; padding-bottom: 0; }
- 	h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+	h1, h2 { color: #00688b; margin-bottom: 0; padding-bottom: 0; }
  	h1 { font-size: 2em; }
  	h2 { font-size: 1.75em; }
  	h3 { font-size: 1.2em; }
- 	div { background-color: #F7F7F7; margin-top: 0.75em; max-width: 400px}
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
-	input { 
+ 	div { background-color: #F7F7F7; margin-top: 0.75em; max-width: 400px; padding: 20; margin: 20}
+	input [type=submit]{ 
 		background-color: #009acd;
 		border: none;
 		color: white;
 		padding: 16px 32px;
 		text-decoration: none;
 		margin: 4px 2px;
+		width: 400px;
 		cursor: pointer;}
  </style>
  </head>
  <body>
  <div>
- <h1>Order Form</h1>
+ <h1>Bob's Auto Parts</h1>
+ <h2>Order Form</h2>
  <p>Please enter your details and then click <strong>Submit</strong> to enter your order.</p>
  <form  action="welcome.php" method="post">
  
