ee58dcd0aba98e117f4afbde64d37e5eae74c739
Luke Mackie <9522654@student.swin.edu.au>
2017-09-26 16:40:42 +1000

    more css

--- index.php
+++ index.php
@@ -11,9 +11,17 @@
  	h1 { font-size: 2em; }
  	h2 { font-size: 1.75em; }
  	h3 { font-size: 1.2em; }
- 	table { background-color: #e5e5e5; margin-top: 0.75em; }
+ 	form { background-color: #e5e5e5; margin-top: 0.75em; }
  	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
  	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+	input[type=submit] { 
+		background-color: #009acd;
+		border: none;
+		color: white;
+		padding: 16px 32px;
+		text-decoration: none;
+		margin: 4px 2px;
+		cursor: pointer;}
  </style>
  </head>
  <body>
