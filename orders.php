97297cd1e5e7400869c5abcf752871704df76c3f
sammut91 <sammut91@gmail.com>
2017-09-27 12:14:03 +1000

    Show columns from table

--- orders.php
+++ orders.php
@@ -54,7 +54,7 @@ while($tableName = mysqli_fetch_row($result)) {
 	$table = $tableName[0];
 	
 	echo '<h3>',$table,'</h3>';
-	$result2 = mysqli_query(.$table, 'SHOW COLUMNS FROM') or die('cannot show columns from '.$table);
+	$result2 = mysqli_query($dbConnection, 'SHOW COLUMNS FROM '.$table) or die('cannot show columns from '.$table);
 	if(mysqli_num_rows($result2)) {
 		echo '<table cellpadding="0" cellspacing="0" class="db-table">';
 		echo '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default<th>Extra</th></tr>';
