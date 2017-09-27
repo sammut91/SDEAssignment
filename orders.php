0d70acf37eb2bd225db9991f8c9135db7b33367c
sammut91 <sammut91@gmail.com>
2017-09-27 12:01:55 +1000

    Add another i

--- orders.php
+++ orders.php
@@ -55,7 +55,7 @@ while($tableName = mysqli_fetch_row($result)) {
 	
 	echo '<h3>',$table,'</h3>';
 	$result2 = mysqli_query('SHOW COLUMNS FROM '.$table) or die('cannot show columns from '.$table);
-	if(mysql_num_rows($result2)) {
+	if(mysqli_num_rows($result2)) {
 		echo '<table cellpadding="0" cellspacing="0" class="db-table">';
 		echo '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default<th>Extra</th></tr>';
 		while($row2 = mysqli_fetch_row($result2)) {
