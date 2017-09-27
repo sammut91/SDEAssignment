10ca78bd787335bc60f7e6a45805c81e764b2824
sammut91 <sammut91@gmail.com>
2017-09-27 12:01:36 +1000

    Use sqli

--- orders.php
+++ orders.php
@@ -46,33 +46,27 @@ else
 {
     echo "Failed to connect to the database.\n";
 }
+
 /* show tables */
+$result = mysqli_query('SHOW TABLES',$connection) or die('cannot show tables');
+while($tableName = mysqli_fetch_row($result)) {
 
-$sql = "show tables;";
-if ($dbConnection->query($sql) === TRUE) {
-    $result = $dbConnection->query($sql);
-    while($tableName = mysql_fetch_row($result)) {
-        
-            $table = $tableName[0];
-            
-            echo '<h3>',$table,'</h3>';
-            $result2 = mysql_query('SHOW COLUMNS FROM '.$table) or die('cannot show columns from '.$table);
-            if(mysql_num_rows($result2)) {
-                echo '<table cellpadding="0" cellspacing="0" class="db-table">';
-                echo '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default<th>Extra</th></tr>';
-                while($row2 = mysql_fetch_row($result2)) {
-                    echo '<tr>';
-                    foreach($row2 as $key=>$value) {
-                        echo '<td>',$value,'</td>';
-                    }
-                    echo '</tr>';
-                }
-                echo '</table><br />';
-            }
-    }
-
-} else {
-    die('cannot show tables');
+	$table = $tableName[0];
+	
+	echo '<h3>',$table,'</h3>';
+	$result2 = mysqli_query('SHOW COLUMNS FROM '.$table) or die('cannot show columns from '.$table);
+	if(mysql_num_rows($result2)) {
+		echo '<table cellpadding="0" cellspacing="0" class="db-table">';
+		echo '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default<th>Extra</th></tr>';
+		while($row2 = mysqli_fetch_row($result2)) {
+			echo '<tr>';
+			foreach($row2 as $key=>$value) {
+				echo '<td>',$value,'</td>';
+			}
+			echo '</tr>';
+		}
+		echo '</table><br />';
+	}
 }
 
 mysqli_close($dbConnection);
